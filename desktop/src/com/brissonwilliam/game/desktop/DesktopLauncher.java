3e37abf62e2bc01c49c58b53cd0211253379d09e
William Brisson <brissonwilliam1@hotmail.com>
2017-05-28 18:13:53 -0400

    First commit on head

---
+++ desktop/src/com/brissonwilliam/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.brissonwilliam.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.brissonwilliam.game.PurpleRain;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new PurpleRain(), config);
+	}
+}
